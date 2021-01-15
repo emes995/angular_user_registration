f93326232deedd8f2a8000f742320b93058c28e0
Edgar C <23481028+emes995@users.noreply.github.com>
2021-01-15 09:30:54 -0500

    Updates

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
-=======
 import { Menu } from '../menu';
 import { MENUS } from '../menus'
->>>>>>> 7a9b848930d8dfbdd86ce47fc21af81a113d7169
 
 @Component({
   selector: 'app-toolbar',
@@ -12,18 +9,12 @@ import { MENUS } from '../menus'
 })
 export class ToolbarComponent implements OnInit {
 
-<<<<<<< HEAD
-  constructor() { }
-
-  ngOnInit(): void {
-=======
   menus: Menu[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
     this.menus = MENUS;
->>>>>>> 7a9b848930d8dfbdd86ce47fc21af81a113d7169
   }
 
 }
